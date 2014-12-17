01cc04f7622cd8127728f0223cfb158b35be08ba
John Ghidiu <jghidiu@solutechnology.com>
2014-12-17 01:09:22 -0500

    -Better console detection -Prepped for build resolvers

--- src/SettingsManager.js
+++ src/SettingsManager.js
@@ -1,6 +1,10 @@
 /*global jQuery:false */
 /*global chrome:false */
 
+// Build User: ${build.user}
+// Version: ${build.version}
+// Build Date: ${build.date}
+
 // TODO: Safe callbacks
 // TODO: Externs
 // TODO: Take a store implementation (and default to chrome.storage.sync)
@@ -9,38 +13,47 @@
 (function(root, factory) {
     'use strict';
 
+    // Try to define a console object
     (function(){
-        if (!window.console) {
-            window.console = {};
-        }
-        // Union of Chrome, FF, IE, and Safari console methods
-        var consoleFunctions = [
-            'log', 'info', 'warn', 'error', 'debug', 'trace', 'dir', 'group',
-            'groupCollapsed', 'groupEnd', 'time', 'timeEnd', 'profile', 'profileEnd',
-            'dirxml', 'assert', 'count', 'markTimeline', 'timeStamp', 'clear'
-        ];
-        // Define undefined methods as no-ops to prevent errors
-        for (var i = 0; i < consoleFunctions.length; i++) {
-            if (!window.console[consoleFunctions[i]]) {
-                window.console[consoleFunctions[i]] = function() {};
+        try {
+            if (!console && (window)) {
+                // Union of Chrome, FF, IE, and Safari console methods
+                var consoleFunctions = [
+                    'log', 'info', 'warn', 'error', 'debug', 'trace', 'dir', 'group',
+                    'groupCollapsed', 'groupEnd', 'time', 'timeEnd', 'profile', 'profileEnd',
+                    'dirxml', 'assert', 'count', 'markTimeline', 'timeStamp', 'clear'
+                ];
+                // Define undefined methods as no-ops to prevent errors
+                for (var i = 0; i < consoleFunctions.length; i++) {
+                    if (!window.console[consoleFunctions[i]]) {
+                        window.console[consoleFunctions[i]] = function() {};
+                    }
+                }
             }
+        } catch(error) {
+            // Not much to do if there is no console
         }
+
     })();
 
+    // Determine the module system (if any)
     if (typeof define === 'function' && define.amd) {
+        // AMD
         define(factory);
     } else {
+        // Node
         if (typeof exports !== 'undefined') {
             module.exports = factory();
         } else {
-            root.SettingsManager = factory();
+            // None
+            root.TemplateManager = factory();
         }
     }
 
 })(this, function() {
     'use strict';
 
-    return function(defaultSettings) {
+    var SettingsManager = function(defaultSettings) {
 
         // TODO: Use jquery for merges?
 
@@ -94,5 +107,11 @@
         };
 
     };
+
+    // Place the version as a member in the function
+    // TODO: Uncomment once build resolvers are enabled
+    // SettingsManager.version = '${build.version}';
+
+    return SettingsManager;
 
 });
