d281255ec2a1f6c84557b6a8abe6b9fd692d256e
John Ghidiu <jghidiu@solutechnology.com>
2014-12-19 02:07:56 -0500

    -Removed obsolete files -Fixed _load bug and _clear

--- src/ChromeExtensionSettingsManager.js
+++ src/ChromeExtensionSettingsManager.js
@@ -65,7 +65,7 @@
         }
 
         var _load = function(successCallback, errorCallback) {
-            chrome.storage.sync.get({}, function(settings){
+            chrome.storage.sync.get(function(settings){
                 if (chrome.runtime.lastError && _isFunction(errorCallback)) {
                     errorCallback.call(null);
                 } else if (_isFunction(successCallback)) {
@@ -89,13 +89,9 @@
                 if (chrome.runtime.lastError && _isFunction(errorCallback)) {
                     errorCallback.call(null);
                 } else {
-                    chrome.storage.sync.set({}, function() {
-                        if(chrome.runtime.lastError && _isFunction(errorCallback)) {
-                            errorCallback.call(null);
-                        } else if(_isFunction(successCallback)) {
-                            successCallback.call(null);
-                        }
-                    });
+                    if(_isFunction(successCallback)) {
+                        successCallback.call(null);
+                    }
                 }
             });
         };
