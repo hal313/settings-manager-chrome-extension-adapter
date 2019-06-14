deba9ef640d45ff70f9d5ae10a576a75972082ba
John Ghidiu <john@rmdashrf.org>
2019-06-14 14:58:16 -0400

    Added docs

---
+++ docs/examples/amd/amd-test.js
@@ -0,0 +1,52 @@
+// Load the module
+require([
+    '../../scripts/SettingsManager.js',
+    '../../scripts/SettingsManagerChromeExtensionAdapter.js'
+], function onLoaded(SettingsManager, SettingsManagerChromeExtensionAdapter) {
+
+    var settingsManagerChromeExtensionAdapter = new SettingsManagerChromeExtensionAdapter.SettingsManagerChromeExtensionAdapter();
+    var settingsManager = new SettingsManager.SettingsManager(settingsManagerChromeExtensionAdapter);
+
+    // Start with a load
+    settingsManager.load(function onLoad(settings) {
+        // Handle the load response and start a save
+        onSettingsLoaded(settings);
+        settingsManager.save({one: 1, two: 'two'}, function onSave() {
+            // Handle the save response and start another load
+            onSettingsSaved();
+            settingsManager.load(function onLoad(settings) {
+                // Handle the load response and start another save
+                onSettingsLoaded(settings);
+                settingsManager.save({three: 3, two: 2}, function onSave() {
+                    // Handle the save response and start another load
+                    onSettingsSaved();
+                    settingsManager.load(function onLoad(settings) {
+                        // Handle the load response
+                        onSettingsLoaded(settings);
+                    }, onError);
+                }, onError);
+            }, onError);
+        }, onError);
+    }, onError);
+
+
+    // The results element
+    var resultsElement = document.getElementById('js-results-section');
+    // Handlers
+    function onSettingsLoaded(settings) {
+        let element = document.createElement('code');
+        element.append(document.createTextNode(`loaded: ${JSON.stringify(settings, null, 2)}\n`));
+        resultsElement.append(element);
+    }
+    function onSettingsSaved() {
+        let element = document.createElement('code');
+        element.append(document.createTextNode(`saved\n`));
+        resultsElement.append(element);
+    }
+    function onError(error) {
+        let element = document.createElement('code');
+        element.append(document.createTextNode(`error: ${error}\n`));
+        resultsElement.append(element);
+    }
+
+});
---
+++ docs/scripts/SettingsManager.es6.js
@@ -0,0 +1,228 @@
+/**
+ * Determines if a value is a function.
+ *
+ * @param {*} candidate the candidate to test
+ * @returns {boolean} true, if candidate is a Function
+ */
+export function isFunction(candidate) {
+    return 'function' === typeof candidate;
+}
+
+/**
+ * Determines if a value is an object.
+ *
+ * @param {*} candidate the candidate to test
+ * @returns {boolean} true, if candidate is an object
+ */
+export function isObject(candidate) {
+    return null !== candidate && 'object' === typeof candidate && !isArray(candidate);
+}
+
+/**
+ * Determines if a value is an array.
+ *
+ * @param {*} candidate the candidate to test
+ * @returns {boolean} true, if candidate is an array
+ */
+export function isArray(candidate) {
+    return Array.isArray(candidate);
+}
+
+/**
+ * Merges two objects; members of the second object take precedence over the first object.
+ *
+ * @param {Object} target an object to merge to
+ * @returns {Object} the target object, populated with the source object's members
+ */
+export function merge(target, source) {
+    // Merge algorithm adapted from:
+    // From (http://stackoverflow.com/questions/171251/how-can-i-merge-properties-of-two-javascript-objects-dynamically)
+
+    let array = isArray(source),
+        dest = array && [] || {};
+
+    if (array) {
+        target = target || [];
+        dest = dest.concat(target);
+        source.forEach(function onItem(e, i) {
+            if ('undefined' === typeof dest[i]) {
+                dest[i] = e;
+            } else if ('object' === typeof e) {
+                dest[i] = merge(target[i], e);
+            } else {
+                if (target.indexOf(e) === -1) {
+                    dest.push(e);
+                }
+            }
+        });
+    } else {
+        if (isObject(target)) {
+            Object.keys(target || {}).forEach(function (key) {
+                dest[key] = target[key];
+            });
+        }
+        Object.keys(source || {}).forEach(function (key) {
+            if (!isObject(source[key]) || !source[key]) {
+                dest[key] = source[key];
+            }
+            else {
+                if (!target[key]) {
+                    dest[key] = source[key];
+                } else {
+                    dest[key] = merge(target[key], source[key]);
+                }
+            }
+        });
+    }
+
+    return dest;
+}
+
+/**
+ * Executes a function.
+ *
+ * @param {Function} fn the function to execute
+ * @param {*[]} args an array of arguments to execute the function with
+ * @param {Object} [context] the optional context
+ * @returns {*} the return value of the function execution, or null if no function is provided
+ */
+export function execute(fn, args, context) {
+    return new Promise((resolve, reject) => {
+        if (isFunction(fn)) {
+            try {
+                resolve(fn.apply(context || {}, args))
+            } catch (error) {
+                reject(error);
+            }
+        }
+        resolve();
+    });
+}
+
+/**
+ * A store implementation in memory.
+ */
+export class InMemoryStore {
+
+    /**
+     * Creates an instance.
+     */
+    constructor() {
+        this.settings = {};
+    }
+
+    /**
+     * Loads values.
+     *
+     * @returns {Promise} resolves with the settings
+     */
+    load() {
+        return Promise.resolve(merge({}, this.settings));
+    };
+
+    /**
+     * Saves values.
+     *
+     * @param {Object} settings the settings to save
+     * @returns {Promise} resolves with the settings
+     */
+    save(settings) {
+        // Assign the settings
+        this.settings = merge(this.settings, settings);
+
+        // Invoke the callback
+        return Promise.resolve(merge({}, this.settings));
+    };
+
+    /**
+     * Clears values.
+     *
+     * @returns {Promise} resolves with empty settings
+     */
+    clear() {
+        this.settings = {};
+
+        return Promise.resolve({});
+    };
+
+};
+
+/**
+ * Implementation for SettingsManager, a class for storing settings.
+ *
+ * @param {Object} [backingStore] optional backing store to wrap
+ */
+export class SettingsManager {
+
+    /**
+     * Creates a SettingsManager instance backed by an optional store.
+     *
+     * @param {Store} [backingStore] optional store implementation to use
+     */
+    constructor(backingStore) {
+        this.backingStore = backingStore || new InMemoryStore();
+    }
+
+    /**
+     * Loads values.
+     *
+     * @param {Function} [successCallback] the callback invoked on success (invoked with the settings)
+     * @param {Function} [errorCallback] the error callback, invoked on failure
+     * @returns {Promise} always resolves (never rejects) after the callback has been invoked
+     */
+    load(successCallback, errorCallback) {
+        return cleanBackingStoreFunctionPromise(this.backingStore.load(), successCallback, errorCallback);
+    };
+
+    /**
+     * Saves values.
+     *
+     * @param {Object} settings the settings to save
+     * @param {Function} [successCallback] the callback invoked on success
+     * @param {Function} [errorCallback] the error callback, invoked on failure
+     * @returns {Promise} always resolves (never rejects) after the callback has been invoked
+     */
+    save(settings, successCallback, errorCallback) {
+        if (!settings || !isObject(settings)) {
+            return cleanPromise(execute(errorCallback, ['"settings" is not an object']));
+        } else {
+            return cleanBackingStoreFunctionPromise(this.backingStore.save(settings), successCallback, errorCallback);
+        }
+    };
+
+    /**
+     * Clears values.
+     *
+     * @param {Function} successCallback the success callback to invoke on success
+     * @param {Function} [errorCallback] the error callback, invoked on failure
+     * @returns {Promise} resolves
+     */
+    clear(successCallback, errorCallback) {
+        return cleanBackingStoreFunctionPromise(this.backingStore.clear(), successCallback, errorCallback);
+    };
+
+}
+
+
+/**
+ * Cleans a promise by adding a no-op then and a no-op catch.
+ *
+ * @param {Promise} promise the promise to clean
+ * @return {Promise} a promise which always resolves nothing
+ */
+function cleanPromise(promise) {
+    return promise.then(() => {}).catch(() => {});
+}
+
+/**
+ * Cleans a promise from a backing store call and invokes the correct callback.
+ *
+ * @param {Promise} backingStorePromise the promise from the backing store function
+ * @param {Function} [successCallback] the success callback (invoked with the result from the backing store function)
+ * @param {Function} [errorCallback] the error callback (invoked with the error from the backing store function)
+ */
+function cleanBackingStoreFunctionPromise(backingStorePromise, successCallback, errorCallback) {
+    return backingStorePromise
+    .then(result => cleanPromise(execute(successCallback, [result])))
+    .catch(error => cleanPromise(execute(errorCallback, [error])));
+}
